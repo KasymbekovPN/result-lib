7c3bc56a501a2aed41294bf89cc7c87de2e978c3
KasymbekovPN <kasymbekovPN@yandex.ru>
2022-04-06 22:34:41 +0300

    1.0.3

--- src/main/java/kpn/lib/i18n/AbstractI18nService.java
+++ src/main/java/kpn/lib/i18n/AbstractI18nService.java
@@ -1,8 +1,10 @@
 package kpn.lib.i18n;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import kpn.lib.result.Result;
 
@@ -26,7 +28,8 @@ public abstract class AbstractI18nService implements I18nService{
     }
 
     private String createDefaultMessage(Result<?> result) {
-        return result.getCode() + " " + List.of(result.getArgs());
+        List<Object> collect = Arrays.stream(result.getArgs()).collect(Collectors.toList());
+        return result.getCode() + " " + collect;
     }
 
     protected abstract Optional<String> translate(Result<?> result, Locale locale);
